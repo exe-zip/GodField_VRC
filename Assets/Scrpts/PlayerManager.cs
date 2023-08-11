c3f34d494ecbbb2c015e4a933d13acd751b07a47
exe-zip <harisamaruki1009@gmail.com>
2023-08-11 15:57:48 +0900

    +playermanager +fieldmanager +player1(object))
    プレイヤーとそれを総括するフィールドの制御スクリプトを作成
    仮でシーンにPlayer1を配置しHP等のパラメータを確認、CardDataBaseの取得も成功

---
+++ Assets/Scrpts/PlayerManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int hp;
+    public int mp;
+    public int money;
+    public bool sick;
+    public int sick_stage;
+    public bool flash;
+    public bool fog;
+    public bool dream;
+    public bool dark;
+    public CardDataBase testcard;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
