73e232f14b617c0ed4f866a4025512c02181e20a
exe-zip <harisamaruki1009@gmail.com>
2023-08-13 15:42:56 +0900

    +DeckManager +Deck(Object)
    デッキとその制御スクリプトを作成
    カードデータとその封入率をインスペクターから入力、それを登録準にIDをつけDeck配列に格納
    これをランダム抽出することで確率を再現する(予定

---
+++ Assets/Scrpts/DeckManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour
+{
+    public CardDataBase[] data_card = new CardDataBase[0];
+    public int[] probability = new int[0];
+    int[] deck;
+    int deck_len=0;
+    void Start()
+    {
+        for(int i = 0; i < data_card.Length; i++)   //Šm—¦•ê”ŒvŽZ
+        {
+            deck_len += probability[i];
+        }
+
+        deck = new int[deck_len];
+
+        int nownum = 0;
+        for (int i = 0; i < data_card.Length; i++)
+        {
+            for(int j = 0; j < probability[i]; j++)
+            {
+                deck[nownum] = i;
+                nownum++;
+            }
+        }
+
+        for(int i = 0; i < deck.Length; i++)
+        {
+            Debug.Log(deck[i]);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
